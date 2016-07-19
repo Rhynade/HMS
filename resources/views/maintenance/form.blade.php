e42ccac7d989ef93d5273f22225fdec842e899ea
Rhynade <e0006912@u.nus.edu>
2016-07-19 12:49:34 +0800

    changed order of routes

--- resources/views/maintenance/form.blade.php
+++ resources/views/maintenance/form.blade.php
@@ -8,68 +8,40 @@
   <script src="{{ URL::to('tinymce/js/tinymce/tinymce.min.js') }}"></script>
   <!-- <script src='//cdn.tinymce.com/4/tinymce.min.js'></script> -->
   <script>
-  tinymce.init({
-    selector: '#mytextarea',
-    statusbar:false,
+    tinymce.init({
+      selector: '#mytextarea',
+      statusbar:false,
 
-  });
+    });
   </script>
 </head>
 
 <body>
 
-<div class="container">
-  <form method="POST" action="/maintenance/received">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    <fieldset class="form-group">
-      <label for="title">Title</label>
-      <textarea name="title" class="form-control" id="title" rows="1"></textarea>
-    </fieldset>
-    <fieldset class="form-group">
-      <label for="faultyArea">Faulty Area</label>
-      <textarea name="faultyArea" class="form-control" id="faultyArea" rows="1"></textarea>
-      <small class="text-muted">eg. Window, Blinds, Lights etc.</small>
-    </fieldset>
+  <div class="container">
+    <form method="POST" action="/maintenance/received">
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+      <fieldset class="form-group">
+        <label for="title">Title</label>
+        <textarea name="title" class="form-control" id="title" rows="1"></textarea>
+      </fieldset>
+      <fieldset class="form-group">
+        <label for="faultyArea">Faulty Area</label>
+        <textarea name="faultyArea" class="form-control" id="faultyArea" rows="1"></textarea>
+        <small class="text-muted">eg. Window, Blinds, Lights etc.</small>
+      </fieldset>
 
-    <fieldset class="form-group">
-      <label for="exampleTextarea">Description</label>
-      <textarea name="description" class="form-control" id="mytextarea" rows="10"></textarea>
-    </fieldset>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-</div>
+      <fieldset class="form-group">
+        <label for="exampleTextarea">Description</label>
+        <textarea name="description" class="form-control" id="mytextarea" rows="10"></textarea>
+      </fieldset>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </body>
 </html>
-      <!-- <div class="checkbox">
-        <label>
-          <input type="checkbox"> Blinds
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Window
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Lights
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Fan
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Table
-        </label>
-      </div>
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Others
-        </label> -->
 
 
 
-        @endsection
+
+@endsection
